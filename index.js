e412f4d0c5ad8d6ba8fa85894c7b44938859b678
Jeffrey Marshall <46502780+MJeffmarsh@users.noreply.github.com>
2019-07-23 23:13:21 -0500

    finished JS; started styling

--- index.js
+++ index.js
@@ -46,17 +46,17 @@ const questions = [
     }
 ];
 
-let currentQuestion = 0;
+let currentQuestion = 1;
 let score = 0;
 
 function renderPrompt () {
- return `<div class="question"><h2>${questions[currentQuestion].prompt}</h2></div>
+ return `<div class="question"><h2>${questions[currentQuestion-1].prompt}</h2></div>
 <form>
  <fieldset>
-     <label class="options"><input type="radio" name="option" value='${questions[currentQuestion].ans1}' required />A. <span >${questions[currentQuestion].ans1}</span></label>
-     <label class="options"><input type="radio" name="option" value='${questions[currentQuestion].ans2}' required />B. <span >${questions[currentQuestion].ans2}</span></label>
-     <label class="options"><input type="radio" name="option" value='${questions[currentQuestion].ans3}' required />C. <span >${questions[currentQuestion].ans3}</span></label>
-     <label class="options"><input type="radio" name="option" value='${questions[currentQuestion].ans4}' required />D. <span >${questions[currentQuestion].ans4}</span></label>
+     <label class="options"><input type="radio" name="option" value='${questions[currentQuestion-1].ans1}' required />A. <span >${questions[currentQuestion-1].ans1}</span></label>
+     <label class="options"><input type="radio" name="option" value='${questions[currentQuestion-1].ans2}' required />B. <span >${questions[currentQuestion-1].ans2}</span></label>
+     <label class="options"><input type="radio" name="option" value='${questions[currentQuestion-1].ans3}' required />C. <span >${questions[currentQuestion-1].ans3}</span></label>
+     <label class="options"><input type="radio" name="option" value='${questions[currentQuestion-1].ans4}' required />D. <span >${questions[currentQuestion-1].ans4}</span></label>
      <button type="submit" class="sub-btn" >Submit</button>
  </fieldset>   
 </form>`;  
@@ -75,46 +75,62 @@ function raiseScore () {
 function quizStart () {
     $('.quiz-box').on('click', ".start-btn",
     function (event) {
-        $('.start').remove();
-        $('.quiz-box').html(renderPrompt());
-        updateQuestionNum();
+        $('.quiz-box').html(renderPrompt()); 
+        $('.currentQuestion').text(1);
     });
 }
 
-let isRight = `<div class="isRight"><h3>Correct!</h3></div>
-    <button class="next-btn" type="button" >Next</button>`;
+function optionRight () { return `<div class="feedback" id="right"><div><h3>Correct!</h3></div>
+    <img src="https://media.giphy.com/media/VSJLRTebfPgnS/200.gif">
+    <button class="cont-btn" type="button">Continue</button></div>`;}
 
+function optionWrong () {
+    return  `<div class="feedback" id="wrong"><div><h3>Hey, listen! The correct answer is ${questions[currentQuestion-1].correct}!</h3></div>
+    <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/bf857e07-ecf2-43db-9045-55fac2cded2c/d97jt0s-7a652102-1a0e-47e5-b057-85692ea4c21b.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2JmODU3ZTA3LWVjZjItNDNkYi05MDQ1LTU1ZmFjMmNkZWQyY1wvZDk3anQwcy03YTY1MjEwMi0xYTBlLTQ3ZTUtYjA1Ny04NTY5MmVhNGMyMWIuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.V-BR6gc8B3OI6HeJGyWD5iKwALNmkRjEygmAI46OWCc">
+      <button class="cont-btn" type="button" >Continue</button></div>`;}  
 
-let isWrong =  `<div class="isWrong"><h3>Wrong! The correct answer is ${questions[currentQuestion].correct}!</h3></div>
-      <button class="next-btn" type="button" >Next</button>`;
-
-function isCorrectOption () {
-    if ($('input:checkd') === `${questions[currentQuestion].correct}`) {
-        return true
-    } else {return false
-    }
-}
-
-function nextQuestion () {
-    $('.quiz-box').on('click', ".next-btn", function (event) {
-        updateQuestionNum();
-        $('.quiz-box').html(renderPrompt());
-         });
-}
+function  quizResults () {
+   return `<div class="feedback">
+        <div><h3>You got ${score}/5 questions right!</h3></div>
+        <button class="restart" type="button">New Game</button>
+    </div>`
+    }      
 
 function feedback () {
     $('.quiz-box').on('submit', function (event) {
         event.preventDefault();
         let markedOption = $('input:checked').val();
-        let correctOption = questions[currentQuestion -1].correct;
+        let correctOption = questions[currentQuestion-1].correct;
         console.log(markedOption, correctOption)
         if (markedOption === correctOption) {
-            $('.quiz-box').html(isRight);
+            $('.quiz-box').html(optionRight());
+            raiseScore();
         } else {
-            $('.quiz-box').html(isWrong);
+            $('.quiz-box').html(optionWrong());
         }
     });
 }
+
+function nextQuestion () {
+    $('.quiz-box').on('click', ".cont-btn", function (event) {
+        if (currentQuestion >= 5) {
+            $('.quiz-box').html(quizResults());
+        } else {
+            updateQuestionNum();
+        $('.quiz-box').html(renderPrompt());
+         }
+    });
+}
+
+function quizRestart () {
+    $('.quiz-box').on('click', ".restart", function (event) {
+        location.reload();
+    });
+}
+
+
+
+
 
 
 
@@ -126,6 +142,7 @@ function runQuiz () {
     quizStart();
     feedback();
     nextQuestion();
+    quizRestart();
 }
 
 $(runQuiz);
